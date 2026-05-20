4ffc5b4992ea75afe42c3f5e48a12fdcf92a2e83
Ramón Hernández <ramon.hernandez@tafersales.com>
2026-05-20 10:45:12 -0600

    feat: add laravel orchestra

---
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Workbench\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WorkbenchServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+         if ($this->app->environment('local')) {
+            // Blast se registra automáticamente vía auto-discovery
+        }
+    }
+}
